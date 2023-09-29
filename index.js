321165c7e786c7b567e5ac8f5c3e36643df6b6ab
Alexandre Ito <shiguemialexandre@gmail.com>
2023-09-29 19:51:34 -0300

    ajuste no tratamento de error

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ function login() {
     }).catch(error => {
         hideLoading();
         console.error('Erro no login: ', error);
-        if (error.code === "auth/wrong-password" || error.code === "auth/invalid-email" || error.code === "auth/invalid-login-credentials") {
-            passwordError.textContent = "Senha inválida. Tente novamente.";
+        if (error.code === "auth/wrong-password" || error.code === "auth/invalid-login-credentials") {
+            passwordError.textContent = "Senha ou email inválida. Tente novamente.";
         } else if (error.code === "auth/too-many-requests"){
             passwordError.textContent = "Por favor, tente novamente mais tarde.";
         } else if (error.code === "auth/missing-password"){
             passwordError.textContent = "Por favor, insira uma senha";
-        }else {
+        } else if (error.code === "auth/invalid-email"){
+            passwordError.textContent = "Por favor, inserir senha e email válidos";
+        } else {
             passwordError.textContent = "";
         }   
     });
