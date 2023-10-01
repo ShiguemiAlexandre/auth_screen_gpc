340b9e02fb411eba92a7b3c8123ecd975683e859
Alexandre Ito <shiguemialexandre@gmail.com>
2023-10-01 18:45:55 -0300

    alteração na arquitetura do projeto e alguns ajustes das configurações do firebase

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDn9hElXSedzi6aOrp0-oYwGXYRrj7oheM",
+    authDomain: "fir-auth-5348f.firebaseapp.com",
+    projectId: "fir-auth-5348f",
+    storageBucket: "fir-auth-5348f.appspot.com",
+    messagingSenderId: "66614747266",
+    appId: "1:66614747266:web:7b8460a58829550f9e549b",
+    measurementId: "G-9KKED1ZGZX"
+};
+firebase.initializeApp(firebaseConfig);
